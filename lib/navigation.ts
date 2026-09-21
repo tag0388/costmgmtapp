daa2095197a6df3a79dd335198fadfe2be4500d9
tag0388 <107834063+tag0388@users.noreply.github.com>
2026-09-21 17:55:48 +1000

    Hide unfinished navigation routes

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,11 +1,10 @@
 export const routeMap = {
-  "project-admin": ["general-info", "line-item-attributes", "access-control"],
+  "project-admin": ["general-info", "line-item-attributes"],
   "cost-management": ["cost-codes", "timephasing", "reporting-periods", "cost-code-attributes", "resource-rates", "bulk-baseline-budget", "bulk-actual-cost", "bulk-cost-to-complete"],
   "change-management": ["change-management", "change-attributes", "bulk-change-records"],
   "subcontract-management": ["subcontract-management", "subcontract-attributes", "bulk-line-items"],
-  "system-admin": ["enterprises", "users", "audit", "settings"],
-  "enterprise-admin": ["settings", "users", "projects", "project-attributes", "line-item-attributes", "cost-code-attributes", "resource-rates", "change-attributes", "subcontract-attributes"],
-  "my-profile": ["details", "preferences", "security"],
+  "system-admin": ["enterprises"],
+  "enterprise-admin": ["settings", "projects", "project-attributes", "line-item-attributes", "cost-code-attributes", "resource-rates", "change-attributes", "subcontract-attributes"],
 } as const;
 
 export type ModuleSlug = keyof typeof routeMap;
